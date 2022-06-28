ca18c3216365572117efa54e0b495c67960c2e93
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-28 16:36:48 -0700

    pokemon query working

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import YelpSearch from './YelpSearch';
 function App() {
   // now make state
   const [pokemon, setPokemon] = useState([]);
+  const [pokemonQuery, setPokemonQuery] = useState([]);
+
 
   // make async function
   useEffect(() => {
@@ -19,20 +21,27 @@ function App() {
 
   }, []);
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
 
+  console.log(pokemonQuery);
+  
   return (
     <div className="App">
-      <div className='App-Header'>
-        {
-          pokemon.map((poke, i) => <div key={poke.pokemon + i}>
-            <p>{poke.pokemon}</p>
-            <img src={poke.url_image}/>
+      <form handleSubmit={handleSubmit}>
+        <input onChange={e => setPokemonQuery(e.target.value)}/>
+        <button>Search</button>
+      </form>
+      {
+        pokemon.map((poke, i) => <div key={poke.pokemon + i} className="pokemon">
+          <p>{poke.pokemon}</p>
+          <img src={poke.url_image}/>
 
-          </div>)
-        }
-        <PokemonSearch />
-        <YelpSearch />
-      </div>
+        </div>)
+      }
+      <PokemonSearch />
+      <YelpSearch />
     </div>
   );
 }
