8708dbe66cbab2e0255b3fbec7b7f2c170b1e5bf
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-28 15:59:20 -0700

    pokemon showing in console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getPokemon } from './services/fetch-utils';
 import PokemonSearch from './PokemonSearch';
 import YelpSearch from './YelpSearch';
 
 function App() {
+  // now make state
+  const [pokemon, setPokemon] = useState([]);
+
+  // make async function
+  useEffect(() => {
+    async function doLoad() {
+      const data = await getPokemon();
+      console.log(data);
+      setPokemon(data);
+    }
+
+    doLoad();
+
+  }, []);
+
+
   return (
     <div className="App">
       <PokemonSearch />
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,4 +1,7 @@
 export async function getPokemon() {
-    const rawData = fetch ('http://localhost:888/.netlify/functions/pokemon');
-    const data = await await (rawData).json();
+  const rawData = await fetch('http://localhost:8888/.netlify/functions/pokemon');
+  const data = await rawData.json();
+  
+
+  return data;
 }
